8feba88530042cfd732f82a2bafb50096a89c48b
IgorSAmelo <103616650+IgorSAmelo@users.noreply.github.com>
2023-02-10 23:41:02 -0300

    a

--- Exe_java/Entidade/TesteProduto.java
+++ Exe_java/Entidade/TesteProduto.java
@@ -9,11 +9,13 @@ public class TesteProduto {
         Produto produto1 = new Produto(1, "TV LCD", 3500, forn1);
         Produto produto2 = new Produto(2, "notebook", 2000, forn2);
         Produto produto3 = new Produto(3, "impressora", 232, forn2);
+        Produto produto4 = new Produto(4, "Som", 552, forn2);
 
         ConjuntoProdutos conjuntoProdutos = new ConjuntoProdutos();
         conjuntoProdutos.inserirProduto(produto1);
         conjuntoProdutos.inserirProduto(produto2);
         conjuntoProdutos.inserirProduto(produto3);
+        conjuntoProdutos.inserirProduto(produto4);
         conjuntoProdutos.inserirProduto(produto1);
         conjuntoProdutos.removerProduto(produto2);
         produto2.setValor(8000);
